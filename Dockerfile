efddbbe69283dc937e5a22a4cfa94ee52be7f6f3
Armando Barbosa <armando.barbosa@getrak.com.br>
2021-05-08 00:28:01 -0300

    Add kafka

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ RUN apk add --no-cache curl bash unzip && \
     curl https://geoftp.ibge.gov.br/organizacao_do_territorio/malhas_territoriais/malhas_municipais/municipio_2019/Brasil/BR/br_municipios_20200807.zip --output /tmp/shape.zip && \
     unzip /tmp/shape.zip -d /tmp/shape && \
     rm /tmp/shape.zip && \
-    pip3 install --no-cache uvicorn==0.13.4 fastapi==0.63.0 && \
+    pip3 install --no-cache uvicorn==0.13.4 fastapi==0.63.0 kafka-python==2.0.2 && \
     conda install --yes --file /tmp/requirements.txt
 
 EXPOSE 8000
--- src/main.py
+++ src/main.py
@@ -1,12 +1,54 @@
-from src.malha.malha import get_address
-from typing import Optional
-from fastapi import FastAPI
+import logging
 import json
 import os
+import sys
 
-app = FastAPI()
+sys.path.append(os.path.dirname(__file__))
+print(os.path.dirname(__file__))
+from kafka import KafkaConsumer, KafkaProducer
+
+try:
+    from src.malha.malha import get_address
+except:
+    from malha.malha import get_address
+
+logging.basicConfig(filename="reverse.log", level=logging.INFO)
+
+from typing import Optional
+from fastapi import FastAPI
 
 
-@app.get("/reverse/{x}/{y}")
-def read_item(x: float, y: float, q: Optional[str] = None):
-    return get_address(x, y)
+def api():
+    app = FastAPI()
+
+    @app.get("/reverse/{x}/{y}")
+    async def read_item(x: float, y: float, q: Optional[str] = None):
+        return get_address(x, y)
+
+
+def kafka_consumer():
+    logging.info("Connecting with kafka")
+    consumer = KafkaConsumer(
+        "locations",
+        group_id="geo_reverse",
+        bootstrap_servers=os.environ["KAFKA_BROKERCONNECT"],
+    )
+    producer = KafkaProducer(
+        bootstrap_servers=os.environ["KAFKA_BROKERCONNECT"],
+        value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+    )
+    logging.info("Conected to kafka")
+    for message in consumer:
+        position = json.loads(message.value)
+        address = get_address(float(position["x"]), float(position["y"]))
+
+        if address is not None:
+            position.update(address)
+            producer.send("locations_processed", position)
+
+        logging.debug(position)
+
+
+if __name__ == "__main__":
+    kafka_consumer()
+    # api()
