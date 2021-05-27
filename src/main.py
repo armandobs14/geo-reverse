f6b4439eeb9fa0ff5d3bbd870eb87e02f8957173
Armando Barbosa <armandobs14@gmail.com>
2021-05-27 00:51:29 -0300

    Feature/add traefik load balancer (#2)
    * Add kafka
    
    * Add kafka
    
    * Add boundingbox as pre-filter
    
    Co-authored-by: Armando Barbosa <armando.barbosa@getrak.com.br>

--- src/main.py
+++ src/main.py
@@ -1,12 +1,41 @@
-from src.malha.malha import get_address
-from typing import Optional
 from fastapi import FastAPI
+import logging
 import json
 import os
+import sys
 
-app = FastAPI()
+
+class App:
+    def api():
+        app = FastAPI()
+
+        @app.get("/reverse/{x}/{y}")
+        async def read_item(x: float, y: float):
+            return get_address(x, y)
+
+        return app
+
+    def kafka_consumer():
+        logging.info("Connecting with kafka")
+        consumer = KafkaConsumer(
+            "locations",
+            group_id="geo_reverse",
+            bootstrap_servers=os.environ["KAFKA_BROKERCONNECT"],
+        )
+        producer = KafkaProducer(
+            bootstrap_servers=os.environ["KAFKA_BROKERCONNECT"],
+            value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+        )
+        logging.info("Conected to kafka")
+        for message in consumer:
+            position = json.loads(message.value)
+            address = get_address(float(position["x"]), float(position["y"]))
+
+            if address is not None:
+                position.update(address)
+                producer.send("locations_processed", position)
+
+            logging.debug(position)
 
 
-@app.get("/reverse/{x}/{y}")
-def read_item(x: float, y: float, q: Optional[str] = None):
-    return get_address(x, y)
+app = App.api() if os.environ["APPLICATTION"] == "API" else App.kafka_consumer()
