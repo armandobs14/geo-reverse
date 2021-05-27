1666d82bafd4b88bb3f0a5002149769a4e42ac2d
Armando Barbosa <armandobs14@gmail.com>
2021-05-27 00:51:29 -0300

    Feature/add traefik load balancer (#2)
    * Add kafka
    
    * Add kafka
    
    * Add boundingbox as pre-filter
    
    Co-authored-by: Armando Barbosa <armando.barbosa@getrak.com.br>

--- src/malha/malha.py
+++ src/malha/malha.py
@@ -1,19 +1,54 @@
 from osgeo import ogr
 import os
 
+
+def update(context_list, index):
+    aux = context_list[index]
+    context_list.remove(aux)
+    context_list.insert(0, aux)
+
+
+def get_bbox(poly):
+    """
+    Returns a polygon of the bounding rectangle of an input polygon.
+    Parameters
+    ----------
+    poly
+        An ogr.Geometry object containing a polygon
+
+    Returns
+    -------
+    An ogr.Geometry object with a four-point polygon representing the bounding rectangle.
+
+    """
+    aoi_bounds = ogr.Geometry(ogr.wkbLinearRing)
+    x_min, x_max, y_min, y_max = poly.GetEnvelope()
+    aoi_bounds.AddPoint(x_min, y_min)
+    aoi_bounds.AddPoint(x_max, y_min)
+    aoi_bounds.AddPoint(x_max, y_max)
+    aoi_bounds.AddPoint(x_min, y_max)
+    aoi_bounds.AddPoint(x_min, y_min)
+    bounds_poly = ogr.Geometry(ogr.wkbPolygon)
+    bounds_poly.AddGeometry(aoi_bounds)
+    return bounds_poly
+
+
 # load shapefile
 driver = ogr.GetDriverByName("ESRI Shapefile")
 shape_path = os.environ["SHAPE_PATH"] if "SHAPE_PATH" in os.environ else "/tmp/shape"
 path = f"{shape_path}/BR_Municipios_2019.shp"
 cityShapefile = driver.Open(path)
 cities = []
+boundaries = []
 
 # loading layers
 cityLayer = cityShapefile.GetLayer()
 
 # caching cities
 for i in range(cityLayer.GetFeatureCount()):
-    cities.append(cityLayer.GetFeature(i))
+    city = cityLayer.GetFeature(i)
+    cities.append(city)
+    boundaries.append(get_bbox(city.geometry()))
 
 
 def get_address(lng: float, lat: float):
@@ -32,19 +67,12 @@ def get_address(lng: float, lat: float):
     point = ogr.Geometry(ogr.wkbPoint)
     point.AddPoint(lng, lat)
 
-    for index, city in enumerate(cities):
-        if city.geometry().Contains(point):
-            aux = cities[index]
-            cities.remove(aux)
-            cities.insert(0, aux)
-            break
+    for index, boundary in enumerate(boundaries):
+        if boundary.Contains(point):
+            if cities[index].geometry().Contains(point):
+                update(cities, index)
+                update(boundaries, index)
+                return cities[0].items()
 
-    if city.geometry().Contains(point):
-        return {
-            "ibge_code": city.GetField("CD_MUN"),
-            "city": city.GetField("NM_MUN"),
-            "uf": city.GetField("SIGLA_UF"),
-            "area_km2": city.GetField("AREA_KM2"),
-        }
-    else:
+    if index == len(cities):
         return None
